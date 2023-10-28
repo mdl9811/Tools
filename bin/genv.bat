4784d727df4f860bb6dc7f3f0274293c63397dac
madelei <1913272757@qq.com>
2023-10-28 17:58:19 +0800

    更新脚本

--- bin/genv.bat
+++ bin/genv.bat
@@ -3,4 +3,4 @@
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 set HTTP_PROXY=http://172.16.1.17:8081
 set HTTPS_PROXY=http://172.16.1.17:8081
-set PATH=C:\tools\depot_tools;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\Program Files\Git\cmd;C:\Program Files\Java\jdk1.8.0_291\bin;C:\Users\19132\.cargo\bin;C:\Tools\bin;C:\Program Files\LLVM\bin;
+set PATH=C:\Tools\src\depot_tools;%PATH%
