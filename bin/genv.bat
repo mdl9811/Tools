203683fda0e5661624389119946a1fd72c9a2f64
madelei <1913272757@qq.com>
2023-10-28 18:58:50 +0800

    增加 gclone ver1.0.0 脚本

--- bin/genv.bat
+++ bin/genv.bat
@@ -1,6 +1,6 @@
 @echo off
 
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
-set HTTP_PROXY=http://172.16.1.17:8081
-set HTTPS_PROXY=http://172.16.1.17:8081
+set HTTP_PROXY=http://127.0.0.1:1080
+set HTTPS_PROXY=http://127.0.0.1:1080
 set PATH=C:\Tools\src\depot_tools;%PATH%
