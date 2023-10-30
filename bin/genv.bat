ee7afc419f12925d616e03e98a286280680dc2ee
madelei <1913272757@qq.com>
2023-10-30 16:03:53 +0800

    更新版本

--- bin/genv.bat
+++ bin/genv.bat
@@ -1,6 +1,6 @@
 @echo off
 
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
-set HTTP_PROXY=http://127.0.0.1:1080
-set HTTPS_PROXY=http://127.0.0.1:1080
+set HTTP_PROXY=%URL_PROXY%
+set HTTPS_PROXY=%URL_PROXY%
 set PATH=%GLOBAL_TOOLS_PATH%\..\src\depot_tools;%PATH%
