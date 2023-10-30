af4feee4468848d57c184b159ee807e5c540d02c
madelei <1913272757@qq.com>
2023-10-30 13:38:21 +0800

    更新 global

--- bin/global.bat
+++ bin/global.bat
@@ -8,10 +8,9 @@ if not defined PARMMENT_1 (
 )
 
 setx "GLOBAL_TOOLS_PATH" %1
-setx "GLOBAL_TOOLS_PATH" %1
 
 echo gloabl tools path:%GLOBAL_TOOLS_PATH% name:GLOBAL_TOOLS_PATH
-set CONFIG_PATH=%GLOBAL_TOOLS_PATH%\config\global\config
+set CONFIG_PATH=%PARMMENT_1%\config\global\config
 
 for /F "tokens=1,*" %%A in (%CONFIG_PATH%) do (
 	echo gloabl name:%%A value:%%B
