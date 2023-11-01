355e0ce9db023f005f947cfc4aed2c1429c1b98d
madelei <1913272757@qq.com>
2023-11-01 11:02:36 +0800

    优化脚本

--- bin/config/gclone/tools-pack/echo-sparse.bat
+++ bin/config/gclone/tools-pack/echo-sparse.bat
@@ -5,23 +5,26 @@ set CONFIG_PATH=%GLOBAL_TOOLS_CONFIG_PATH%\config\\gclone\tools-pack\config
 set OPUPUT_PATH=%1\sparse-checkout
 set TARGETD=%4
 
-del %OPUPUT_PATH%
+if exist %OPUPUT_PATH% del %OPUPUT_PATH%
 echo ---------------------------------------------------------------------------------
 echo OPUPUT_PATH:%OPUPUT_PATH%
+
 :next-arg
 if "%1"=="" goto args-done
-if /i "%1"=="--target" goto target
-
+if "%1"=="--target" goto target-args
 shift
 goto next-arg
+
+:target-args
+if "%1"=="" goto args-done
+if "%1"=="--target" shift
+echo /%1/ >> %OPUPUT_PATH% & echo download *%1 
+shift
+goto target-args
 
 :args-done
 echo ---------------------------------------------------------------------------------
 exit /b 0
 
-:target
-if "%1"=="" goto args-done
-if /i "%1"=="--target" shift
-echo /%1/ >> %OPUPUT_PATH% & echo download *%1 
-shift
-goto :target
+
+
